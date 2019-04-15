191714a14a47ced50e9786a97efc3babb7e2275f
박민지 (Park_Minji) <34977602+Park-New-project@users.noreply.github.com>
2019-04-15 23:57:40 +0900

    3 Stack

--- Code.cpp
+++ Code.cpp
@@ -1,117 +1,152 @@
-
-//Queue _ Breadth First Search
-
+//Queue _ Breadth First Search (G_List, stack)
 #include <iostream>
 #include <queue>
+#include <stack>
 using namespace std;
 
-const int X = 9;
-queue<int*> q;
+queue<int**> q;
+int n = 0;	// index of son
+int m = -1;	// index of parent
+int ***G_List = new int**[100];
+int *G_Parent = new int[100];
 
-void push_Queue(int &number, int* State, int newOne[][10], int*currentState, int min, int to) {
-	int i, j = 0, temp;
-	int cnt=0;
+void Sons(int, int, int**);
+void PrintArr(int**);
+int** CopyArr(int **);
+void Put_G_List(int **);
+bool Equal_G_List(int **);
 
-	temp = State[min];
+int main()
+{
+	int **firstArr = new int*[3];
+	int **currentArr;
+	int **arr;
 
-	i = to;
-	while (i < to+3) {
-		if (State[i] == X && i == to) {
-			State[min] = X;
-			State[i] = temp;
-			for (j = 0; j < 10; j++) newOne[number][j] = State[j];
-			q.push(newOne[number]);
-			number++;
-			number = number % 25000;
-
-			
-			for (j = 0; j < 10; j++) State[j] = currentState[j];
-			break;
-		}
-		if (i == to) i++;
-		if (State[i] == X && State[i - 1] != X && State[i - 1] > temp) {
-			if (State[i - 1] != State[min]) State[min] = X;
-			State[i] = temp;
-			for (j = 0; j < 10; j++) newOne[number][j] = State[j];
-			q.push(newOne[number]);
-			number++;
-			number = number % 25000;
-
-			for (j = 0; j < 10; j++) State[j] = currentState[j];
-			break;
-		}
-		i++;
+	// init Arr, q
+	for (int i = 0; i < 3; i++)
+	{
+		firstArr[i] = new int[3];
+		firstArr[0][i] = 3 - i;
 	}
-	return;
-}
-int main() {
-	int *firstState = new int[9]{ 3,2,1,X,X,X,X,X,X };
-	int *finalState = new int[9]{ X,X,X,X,X,X,3,2,1 };
-	int *currentState = new int[10];
-	bool Goal = false;
-	int i, j = 0;
-	int newOne[25000][10];
-	int number = 0;
 
-	q.push(firstState);
+	for (int i = 1; i <= 2; i++)
+		for (int j = 0; j <= 2; j++)
+			firstArr[i][j] = 0;
+	Put_G_List(firstArr);
+	q.push(firstArr);
+
+	// i: start j: end
 	while (!q.empty()) {
-		currentState = q.front();
+		currentArr = q.front();
 		q.pop();
-		currentState[9] = X;
-
-		//goal=true
-		while (j < 10) {
-			if (finalState[j] != currentState[j]) {
-				Goal = false;
-				break;
+		m++;
+		if (currentArr[2][0] == 3 && currentArr[2][1] == 2 && currentArr[2][2] == 1) {
+			stack<int> s;
+			s.push(m);
+			while (m != 0) {
+				s.push(G_Parent[m]);
+				m = G_Parent[m];
 			}
-			Goal = true;
-			j++;
-		}
-
-		if (Goal) {
-			cout << "End!" << endl;
+			int a;
+			while (!s.empty()) {
+				a = s.top();
+				s.pop();
+				PrintArr(G_List[a]);
+			}
+			cout << "end!" << endl;
+			return 0;
 		}
 		else {
-			// 1)
-			int min[3] = { X,X,X };
-			int j = 0;
-			while (j < 3)
-			{
-				if (currentState[j] < currentState[min[0]]) min[0] = j;
-				if (currentState[j + 3] < currentState[min[1]]) min[1] = j + 3;
-				if (currentState[j + 6] < currentState[min[2]]) min[2] = j + 6;
-				j++;
-			}
-
-			// 2) 
-			if (min[0] >= 3) min[0] = X;
-			if (min[1] >= 6) min[1] = X;
-			if (min[2]) min[2] = X;
-
-			cout << number << " : " << min[0] << " " << min[1] << " " << min[2] << endl;
-
-			// 3)
-			int *State = new int[10];
-			for (i = 0; i < 10; i++) State[i] = currentState[i];
-			
-
-			//  A
-			if (min[0] != X) {
-				push_Queue( number, State, newOne, currentState, min[0],3 );
-				push_Queue( number, State, newOne, currentState, min[0], 6);
-			}
-			//  B
-			if (min[1] != X) {
-				push_Queue( number, State, newOne, currentState, min[1],0);
-				push_Queue(number, State, newOne, currentState,  min[0], 6);
-			}
-			//  C
-			if (min[2] != X) {
-				push_Queue( number, State, newOne, currentState, min[2],0);
-				push_Queue( number, State, newOne, currentState, min[0], 3);
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					arr = CopyArr(currentArr);
+					if (i != j) Sons(i, j, arr);
+				}
 			}
 		}
 	}
 	return 0;
+}
+void Sons(int i, int j, int **arr) {
+	int from;
+	int to;
+
+	// start: [i,from]
+	for (from = 2; from >= 0; from--)
+		if (arr[i][from] != 0) break;
+
+	// end: [j,to]
+	for (to = 2; to >= 0; to--)
+		if (arr[j][to] != 0) break;
+	to++;
+
+	// do not start X
+	if (from < 0) {
+		return;
+	}
+
+	// end : 1,2
+	if (to != 0) {
+		if (arr[j][to - 1] <= arr[i][from]) {
+			return;
+		}
+	}
+	int temp = arr[i][from];
+	arr[j][to] = temp;
+	arr[i][from] = 0;
+	if (!Equal_G_List(arr)) {
+		Put_G_List(arr);
+		q.push(arr);
+	}
+}
+int** CopyArr(int **a)
+{
+	int **copy = new int*[3];
+	for (int i = 0; i < 3; i++)
+	{
+		copy[i] = new int[3];
+		for (int j = 0; j < 3; j++) {
+			copy[i][j] = a[i][j];
+		}
+	}
+	return copy;
+}
+void Put_G_List(int **a)
+{
+	G_List[n] = new int*[3];
+	for (int i = 0; i < 3; i++) {
+		G_List[n][i] = new int[3];
+		for (int j = 0; j < 3; j++) {
+			G_List[n][i][j] = a[i][j];
+		}
+	}
+	G_Parent[n] = m;
+	n++;
+}
+bool Equal_G_List(int **a) {
+	int cnt = 0;
+	for (int x = 0; x < n;) {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (G_List[x][i][j] == a[i][j]) {
+					cnt++;
+				}
+			}
+		}
+		if (cnt == 9) return true;
+		else {
+			x++;
+			cnt = 0;
+		}
+	}
+	return false;
+}
+void PrintArr(int **a) {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
 }
